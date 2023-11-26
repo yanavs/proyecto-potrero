a94a2afe11f778b6cc394084f07e47b8cb811f53
Yanina Aviles <yaninaviles@gmail.com>
2023-11-26 13:57:57 -0300

    libreria confetti

---
+++ main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const jsConfetti = new JSConfetti()
+
+    jsConfetti.addConfetti({
+        emojis: ['🌈', '❤️', '✨', '🐾', '🐱', '🐶'],
+    })
+})
